e8bd3466da4a03c17675c774c95d40a817684758
BowDown097 <42720004+BowDown097@users.noreply.github.com>
2022-01-23 17:06:31 -0800

    fuck me

--- base/start.lua
+++ base/start.lua
@@ -1,6 +1,11 @@
-mod_metadata("version", "1.0.0")
-
 local json = require("mods/base/json")
+
+-- metadata about the base mod
+if current_mod_path == "mods/base" or current_mod_path == "mods\\base" then
+    mod_metadata("name", "Base Mod")
+    mod_metadata("version", "1.0.0")
+    mod_metadata("author", "the B2L team")
+end
 
 -- helper for dealing with json data
 function jet_json_patch(path, callback)
@@ -13,4 +18,5 @@ function jet_json_patch(path, callback)
     end)
 end
 
+-- database of file paths
 database = require("mods/base/database")
