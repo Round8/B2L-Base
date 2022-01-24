a449533e30f0fe01ee0301972fb7f3d4b554ef54
BowDown097 <btd6modsyeet@gmail.com>
2022-01-24 15:52:50 -0800

    Version 1.1.0

--- base/start.lua
+++ base/start.lua
@@ -1,20 +1,33 @@
-local json = require("mods/base/json")
-
 -- metadata about the base mod
 if current_mod_path == "mods/base" or current_mod_path == "mods\\base" then
     mod_metadata("name", "Base Mod")
-    mod_metadata("version", "1.0.0")
+    mod_metadata("version", "1.1.0")
     mod_metadata("author", "the B2L team")
 end
 
 -- helper for dealing with json data
 function jet_json_patch(path, callback)
     jet_text_patch(path, function(contents)
-        local parsed = json.decode(contents)
+        local parsed = rapidjson.decode(contents)
 
         parsed = callback(parsed)
 
-        return json.encode(parsed)
+        return rapidjson.encode(parsed)
+    end)
+end
+
+function jet_json_advanced_patch(path, callback)
+    jet_text_patch(path, function(contents)
+        local document = rapidjson.Document()
+
+        local ok, err = document:parse(data)
+        if not ok then
+            error(err)
+        end
+
+        callback(document)
+
+        return document:stringify()
     end)
 end
 
